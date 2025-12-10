b59b4d10a90590a2814ea30d4fea1a9fe59e87f3
Victor <bouffart.victor@gmail.com>
2025-12-10 19:17:09 +0100

    Update code_breaking_bad.py

--- code_breaking_bad.py
+++ code_breaking_bad.py
@@ -7,6 +7,7 @@ choix0 = (0, 1, 2)  ### Racourci pas dans le vrai code ###
 choix2 = (1, 2)
 choix3 = (1, 2, 3)
 choix4 = (1, 2, 3, 4)
+menu = (1, 2, 3, 4, 5, 6)
 
 # Toutes les fonctions #
 
@@ -166,6 +167,93 @@ def mission_dealeur_1(money):
 
 
 def blue_crystal(money, blue_crystal):
+    purete = 50     # base
+    quantite = 5 # base
+    print("\n🔹 Étape 1 : Choix de l’éphédrine")
+    print("1) Pseudoéphédrine industrielle (bon marché)")
+    print("2) Phénylacétone pharmaceutique (qualité ++)")
+    print("3) Méthylamine pure (la spéciale de Walter)")
+
+    choix = demander_choix(" 🔹Ton choix : ", choix3)
+	cout = 5000
+	if money < cout:
+        print("\n❌ Tu n'as pas assez d'argent pour les ingrédients !")
+        return money, 0
+    money -= cout
+
+    if int(choix) == 1:
+        purete += 5
+        quantite += 50
+    elif int(choix) == 2:
+        purete += 15
+        quantite += 30
+    elif int(choix) == 3:
+        purete += 30
+        quantite += 80
+    # --- ÉTAPE 2 : Mode de chauffage ---
+    print("\n🔹 Étape 2 : Mode de chauffage")
+    print("1) Chauffage rapide (volume +, pureté -)")
+    print("2) Chauffage lent et contrôlé (pureté +++)")
+    print("3) Chauffage normal")
+
+    choix = demander_choix("🔹 Ton choix : ", choix3)
+    if int(choix) =="1":
+        purete -= 10
+        quantite += 60
+    elif int(choix) =="2":
+        purete += 25
+        quantite -= 10
+    elif int(choix) == "3":
+        purete += 10
+    # --- ÉtAPE 3 : Technique de cristallisation ---
+    print("\n🔹 Étape 3 : Cristallisation")
+    print("1) Cristallisation simple (classique)")
+    print("2) Cristallisation sous vide (pureté +)")
+    print("3) Cristallisation ultra lente (pureté +++, quantité -)")
+	choix = demander_choix("🔹 Ton choix : ", choix3)
+    if int(choix) == 1:
+        purete += 10
+    elif int(choix) == 2:
+        purete += 20
+    elif int(choix) == 3:
+        purete += 35
+        quantite -= 30
+    # Résultat final
+    print("\n=== RÉSULTAT  ===")
+    print(f" Quantité obtenue : {quantite}kg")
+    print(f" Pureté : {purete}%")
+    print(f"💰 Argent restant : {money}$")
+    print("\nTon lot de CRISTAL BLEU est prêt. Gus va s'en souvenir...")
+
+    return money, quantite
+
+def menu_principal(money, lieu, blue_crystal):
+	while True:
+		print("\n=== 📦 MENU DE TON EMPIRE ===")
+		print("1) Cook de la METH")
+		print("2) Vendre ta production")
+		print("3) Acheter du matériel / armes")
+		print("4) Partir en mission")
+		print("5) Voir ton stock")
+		print("6) Quitter le jeu")
+		menu = demander_choix("🔹 Ton choix : ", menu)
+		if int(menu) == "1":
+			money, blue_crystal = blue_crystal(money, blue_crystal)
+		elif int(menu)) == "2":
+			print("Fonction vente encore à coder.")
+		elif int(menu) == "3":
+			print("Boutique pas encore disponible.")
+		elif int(menu) == "4":
+			print("Missions en travaux.")
+		elif int(menu) == "5":
+			print(f"\n💼 Stock actuel : {blue_crystal}kg")
+			print(f"💰 Argent : {money}$")
+        elif int(menu) == "6":
+            print("Tu quittes ton empire...")
+            fin_histoire()
+    
+    return money, lieu, blue_crystal
+
 
 def intervention_GUS(money, prix, lieu, name):
 	print("\n🐓📞 *Un téléphone sonne au loin...*")
@@ -307,8 +395,9 @@ def baron_de_la_drogue(money):
 	time.sleep(1.5)
 	print("\nTu enfiles ta combinaison jaune. Le moment est venu de cook.")
 	input(">>> Appuie sur Entrée pour commencer la première production...")
-	money, blue_crystal = blue_crystal(money, blue_crytal)
 	
+	money, blue_crystal = menu_principal(money, blue_crystal)
+
 	return money
 
 
